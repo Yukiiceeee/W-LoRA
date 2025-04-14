b0587826b4c47cf196a362f331b32d50e2accfa2
Yukiiceeee <13336340366@163.com>
2025-04-14 21:23:20 +0800

    second commit

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,16 +6,16 @@ python ../src/adapter_train.py \
     --use_deepspeed false \
     --model_name_or_path /d2/mxy/Models/Qwen2-7B \
     --data_name medmc \
-    --output_dir /d2/mxy/W-LoRA/adapters/domain/med/mc_2 \
+    --output_dir /d2/mxy/W-LoRA/adapters/domain/med/mc \
     --peft_type lora \
     --lora_r 8 \
     --lora_alpha 16 \
     --lora_dropout 0.1 \
-    --learning_rate 1e-7 \
+    --learning_rate 2e-6 \
     --lr_scheduler_type "cosine" \
     --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 16 \
-    --gradient_accumulation_steps 8 \
+    --gradient_accumulation_steps 32 \
     --warmup_ratio 0.1 \
     --model_max_length 512 \
     --evaluation_strategy "steps" \
